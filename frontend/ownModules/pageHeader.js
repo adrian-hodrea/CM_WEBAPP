f32728de4908fcb08456b841cb6ec83c0c8e9e31
Adrian Hodrea <adihodrea@yahoo.com>
2019-10-04 10:33:19 +0300

    display the submenues from second level on, on the right from de parent menu

--- frontend/ownModules/pageHeader.js
+++ frontend/ownModules/pageHeader.js
@@ -15,61 +15,73 @@ const renderMenuTree = () => {
     const menuTreeApi = root + "/getMenuTree";
     var menuContainer = document.getElementById("menuTreeContainer");
     menu.getMenuList(menuTreeApi).then( () => {
-        renderTreeToHTML(menuContainer,menu.items);
+        var menuTree = menu.items;
+        renderTreeToHTML(menuContainer,menuTree,"mainMenu");
     });
+}; // end of renderMenuTree
 
-    function renderTreeToHTML(menuContainer,menuTree) {
-        var ulElement = document.createElement("ul");
-        menuTree.forEach( (element, index) => {
-            var menuElement = document.createElement("li");
-            menuElement.setAttribute("menuReleased","false");
-            menuElement.setAttribute("data-id",`${element.id}`);
-            if (element.id < 0) { // Horizontal Main menu item
-                menuElement.innerText = `${element.name}`;
+function renderTreeToHTML(menuContainer,menuTree,menuType, ownerOfMenu) {
+    var ulElement = document.createElement("ul");
+    if (menuType !== 'mainMenu') {
+        if (ownerOfMenu === 'mainMenu') {
+            ulElement.classList.add("firstLevelMenu");          // apply different design(vertical) on submenu then main menu
+        }  else {
+            ulElement.classList.add("secondLevelMenu");          // apply different design(vertical) on submenu then main menu           
+        }               
+    }
+    menuTree.forEach( (elementObj, index) => {
+        var liItemElement = document.createElement("li");
+        var menuItemElement = document.createElement("span");
+        liItemElement.appendChild(menuItemElement);
+        liItemElement.setAttribute("menuReleased","false");
+        liItemElement.setAttribute("data-id",`${elementObj.id}`);
+        if (menuType ==="mainMenu") { // Horizontal Main menu item
+            menuItemElement.innerText = `${elementObj.name}`;
+            if (index < menuTree.length - 1) {
+                liItemElement.style.borderRight = "1px solid lightgrey";
+            }
+        }
+            else {  // Vertical submenu item
                 if (index < menuTree.length - 1) {
-                    menuElement.style.borderRight = "1px solid lightgrey";
-                }
-            }
-                else {  // Vertical submenu item
-                    if (index < menuTree.length - 1) {
-                        menuElement.style.borderBottom = "1px solid lightgrey";
-                    }    
-                    menuElement.innerHTML = `
-                        <span class="menuItemType${element.type}">${element.type}</span>    
-                        ${element.name}
-                        <i class="fas fa-angle-right"></i>
-                    `;
-                    ulElement.classList.add("firstLevelMenu");
+                    liItemElement.style.borderBottom = "1px solid lightgrey";
                 }    
-            ulElement.appendChild(menuElement);
-            if (element.children) {
-                menuElement.addEventListener("click",createSubmenuOnClick(menuElement));
+                menuItemElement.innerHTML = `
+                    <span class="menuItemType${elementObj.type}">${elementObj.type}</span>    
+                    <span class="menuItemText">${elementObj.name}</span>
+                    <i class="fas fa-angle-right"></i>
+                `;
             }    
+        ulElement.appendChild(liItemElement);
+        if (elementObj.children) {
+            menuItemElement.addEventListener("click",createSubmenuOnClick());
+        }    
 
-            function createSubmenuOnClick(menuItem) {
-                return () => {
-                    var releasedMenu = menuItem.getAttribute("menuReleased");
-                    removeSiblingMenu();
-                    if (releasedMenu === "false") {
-                        menuElement.setAttribute("menuReleased","true");
-                        renderTreeToHTML(menuElement,element.children);    
-                    }
+        function createSubmenuOnClick() {
+            return () => {
+                var releasedMenu = menuItemElement.parentElement.getAttribute("menuReleased");
+                removeSiblingMenu();
+                if (releasedMenu === "false") {
+                    menuItemElement.parentElement.setAttribute("menuReleased","true");
+                    renderTreeToHTML(liItemElement,elementObj.children,"subMenu",menuType);    
                 }
             }
 
-            function removeSiblingMenu() {
-                var openedMenuItem = ulElement.querySelector('li[menuReleased="true"]');
-                if (openedMenuItem) {
-                    var submenu = openedMenuItem.querySelector('ul');
-                    submenu.parentElement.removeChild(submenu); 
-                    openedMenuItem.setAttribute("menuReleased","false");   
-                }
-            }
 
-        });
-        menuContainer.appendChild(ulElement);
-    } ; 
+
+        }
+
+        function removeSiblingMenu() {
+            var openedMenuItem = ulElement.querySelector('li[menuReleased="true"]');
+            if (openedMenuItem) {
+                var submenu = openedMenuItem.querySelector('ul');
+                submenu.parentElement.removeChild(submenu); 
+                openedMenuItem.setAttribute("menuReleased","false");   
+            }
+        }
+
+    });
+    menuContainer.appendChild(ulElement);
+} ; 
     
-};
 
 export {renderPageHeader,renderMenuTree};
